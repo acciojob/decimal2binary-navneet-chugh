fac8ac0f9acbabb5d7d9175f1b80c52745c5426a
navneet-chugh <navneetchugh15@gmail.com>
2023-08-14 13:21:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function decimalToBinary(num) {
-  //Write you code here
+    //Write you code here
+    let binary = "";
+    while(num>0){
+        binary = (num%2)+binary;
+        num=Math.floor(num/2);
+    }
+    if(binary===""){
+        binary = "0";
+    }
+    return binary;
+  }
   
-}
-
-window.decimalToBinary = decimalToBinary;
+  window.decimalToBinary = decimalToBinary;
+  
